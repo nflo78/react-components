3734e541fc01d42bfad1e1c54aa370eaae122375
Noriel <flores1noriel@gmail.com>
2023-04-10 21:51:30 -0700

    Added grocery list and click handler functions

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-// TODO
+const { useState } = React;
+const onListItemClick = (event) => {
+  console.log('I got clicked');
+};
+const TodoListItem = (props) => {
+  const [isDone, setIsDone] = useState(false);
+
+  const style = {
+    textDecoration: isDone ? 'line-through' : 'none'
+  };
+  return (
+  <li style={style} onClick={() => setIsDone(!isDone)}>{props.todo}</li>
+  )
+};
+
+const GroceryItems = (props) => {
+  const [isDone, setIsDone] = useState(false);
+
+  const style = {
+    fontWeight: isDone ? 'bold' : 'normal'
+  };
+
+  return (
+    <li style={style} onClick={() => setIsDone(!isDone)}>{props.grocery}</li>
+  )
+};
+
+const TodoList = (props) => {
+  return (
+    <ul>
+    {props.todos.map((todo) => (
+      <TodoListItem todo={todo} />
+    ))}
+  </ul>
+  )
+};
+
+const groceries = ['apple', 'kale', 'berries']
+const GroceryList = (props) => {
+  return (
+    <ul>
+    {props.GroceryListItems.map((grocery) => (
+      <GroceryItems grocery={grocery} />
+    ))}
+  </ul>
+  )
+}
+
+const App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
+    <h3>Grocery List</h3>
+    <GroceryList GroceryListItems={groceries}/>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"))
